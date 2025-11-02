a1161b5fb2e64fa90686586ecd45353e59aecbf7
Monetta22 <150975815+Monetta22@users.noreply.github.com>
2025-11-02 17:11:28 -0600

    adding connection to db

---
+++ JapaneseRestaurant/Connection/Connection.cs
@@ -0,0 +1,16 @@
+﻿namespace JapaneseRestaurant.Conection
+{
+    internal class Connection
+    {
+        private string server = "";
+        private string database = "";
+        private string user = "";
+        private string password = "";
+        private string connection = "";
+
+        public Connection()
+        {
+
+        }
+    }
+}
